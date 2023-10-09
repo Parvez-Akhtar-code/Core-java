c6294ebc8873cfccdeb2cb599cbdc06cc11b08c0
Parvez Akhtar <parvezakhtar310@gmail.com>
2023-10-09 18:23:39 +0530

    First Commit

---
+++ IOStreams/src/IOStream/CopyImage.java
@@ -0,0 +1,21 @@
+package IOStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class CopyImage {
+
+	public static void main(String[] args) throws Exception {
+		FileInputStream fis = new FileInputStream("F:/xyz/Sunset.jpg");
+		int size = fis.available();
+		byte[] b = new byte[size];
+		fis.read(b);
+		
+		FileOutputStream fos = new FileOutputStream("F:/abc/Sunset1.jpg");
+		fos.write(b);
+		System.out.println("file copied successfully");
+		
+
+	}
+
+}
